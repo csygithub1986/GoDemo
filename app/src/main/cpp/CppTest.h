d4585c210d67654552de60ced6e9c413bc46e26c
csy <274322346@qq.com>
2017-09-06 00:11:41 +0800

    临时家里提交

---
+++ app/src/main/cpp/CppTest.h
@@ -0,0 +1,105 @@
+#pragma once
+
+#include <iostream>
+#include<map>
+#include <vector>
+#include <list>
+#include <math.h>
+#include "opencv2/opencv.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/cvconfig.h"
+#include "opencv/cv.h"
+#include "opencv/cxcore.h"
+#include "opencv/highgui.h"
+#include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
+
+
+using namespace cv;
+using namespace std;
+//using cv::Mat;
+//using cv::Vec3f;
+//using cv::Point2f;
+//using cv::Point;
+
+//#define EX extern "C" __declspec(dllexport)
+//#define EX extern "C"
+#define GetArrayLen(x) (sizeof((x)) / sizeof(x)[0])
+
+
+template<class T>
+int getArrayLen(T &array) {
+    return (sizeof(array) / sizeof(array[0]));
+}
+
+
+//结构体
+struct CircleF {
+    Point2f Center;
+    float Radius;
+
+    CircleF() {}
+
+    CircleF(Point2f center, float radius) {
+        Center = center;
+        Radius = radius;
+    }
+};
+
+struct LineSegment2DF {
+    Point2f P1;
+    Point2f P2;
+    //double Length;
+    Point2f Direction;
+
+    LineSegment2DF() {}
+
+    LineSegment2DF(Point2f p1, Point2f p2) {
+        P1 = p1;
+        P2 = p2;
+        Direction.x =
+                (p2.x - p1.x) / sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
+        Direction.y =
+                (p2.y - p1.y) / sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
+    }
+};
+
+//枚举
+enum CrossType {
+    None = 0, Left, Up, Right, Down, LeftUp, RightUp, RightDown, LeftDown, Center
+};
+
+
+//导出函数
+void Detect(unsigned char *, int, int, int, int, int[]);
+
+void SetConfig(double, double, double, double, double, double);
+
+void GetCoordinate(int[], int[]);
+
+
+//内部函数
+Mat InitImage(Mat);
+
+vector<CircleF> DetectCircle(Mat uimage, int boardSize);
+
+map<CrossType, list<Point>> DetectCross(uchar *imageBytes, int width, int height);
+
+void LineFit(vector<Point2f> points, Point2f *direction, Point2f *pointOnLine);
+
+Point2f
+FindLineCross(Point2f direction1, Point2f pointOnLine1, Point2f direction2, Point2f pointOnLine2);
+
+Point *GetGridCoordinate(LineSegment2DF *horizontalLines, LineSegment2DF *verticalLines);
+
+void GetEvenDevideLines(Point2f *conors, Point2f directionLeft, Point2f directionRight,
+                        Point2f directionUp, Point2f directionDown, LineSegment2DF *horizontalLines,
+                        LineSegment2DF *verticalLines);
+
+Point2f *FindConor(Point2f *directionLeft, Point2f *directionRight, Point2f *directionUp,
+                   Point2f *directionDown);
+
+int FindStone(int index, uchar *cannyBytes, uchar *grayImageData);
+
+
+
